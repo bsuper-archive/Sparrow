fb1f13ab1a8de7421fdabce678ca436bf59ae6a7
Travis CI <aschmitt@berkeley.edu>
2016-01-31 15:00:31 -0800

    found bluetooth MAC. Discovers Devices and list displays on screen

--- app/src/main/java/me/bsu/brianandysparrow/MainActivity.java
+++ app/src/main/java/me/bsu/brianandysparrow/MainActivity.java
@@ -19,20 +19,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "me.bsu.MainActivity";
 
+    // UI
     TextView macAddressTextView;
     EditText messageEditText;
     Button sendMessageButton;
     ListView deviceListView;
 
+    // Bluetooth
     BluetoothAdapter mBluetoothAdapter;
+    String adapterName;
     private boolean bluetoothReady = false;
+    Set<BluetoothDevice> availableDevices;
+    List<String> displayableDevices;
+    Boolean DEBUG = true;
 
     // Create a BroadcastReceiver for ACTION_FOUND
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -43,8 +51,13 @@ public class MainActivity extends AppCompatActivity {
                 // Get the BluetoothDevice object from the Intent
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // Add the name and address to an array adapter to show in a ListView
-                Log.d(TAG, "Discoverable device: " + device.getName() + "\n" + device.getAddress());
-                //                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                String deviceStr = deviceToString(device);
+                if (DEBUG) {
+                    Log.d(TAG, "Discovered device: " + deviceStr);
+                }
+                availableDevices.add(device);
+                displayableDevices.add(deviceToString(device));
+                displayDevicesList(displayableDevices);
             }
         }
     };
@@ -55,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         macAddressTextView = (TextView) findViewById(R.id.my_mac_address_text_view);
-        macAddressTextView.setText(getMacAddress());
         messageEditText = (EditText) findViewById(R.id.message_edittext);
         sendMessageButton = (Button) findViewById(R.id.message_send_button);
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
@@ -67,13 +79,15 @@ public class MainActivity extends AppCompatActivity {
         });
         deviceListView = (ListView) findViewById(R.id.devices_list_view);
 
-
         setupBluetooth();
-
-
 
     }
 
+    /**
+     * This gets the wifi mac address which is probably not what we want
+     *
+     * @return
+     */
     private String getMacAddress() {
         WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = manager.getConnectionInfo();
@@ -88,13 +102,15 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
 
     private void setupBluetooth() {
+        if (DEBUG) {
+            Log.d(TAG, "Setting up bluetooth");
+        }
         if (!bluetoothReady) {
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             if (mBluetoothAdapter == null) {
                 // Device does not support Bluetooth
                 Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
-            }
-            if (!mBluetoothAdapter.isEnabled()) {
+            } else if (!mBluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             } else {
@@ -104,47 +120,94 @@ public class MainActivity extends AppCompatActivity {
         } else {
             findDevices();
         }
+    }
+
+    private void displayDevicesList(List<String> devices) {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item_device, devices);
+        deviceListView.setAdapter(arrayAdapter);
+    }
+
+    /**
+     * Attempts to set our name to NAME - MAC, but if NAME is null then only MAC
+     */
+    private void setAdapterName() {
+        String name = mBluetoothAdapter.getName();
+        if (name == null) {
+            name = mBluetoothAdapter.getAddress();
+        } else {
+            name = name + " - " + mBluetoothAdapter.getAddress();
+        }
+
+        adapterName = name;
+        macAddressTextView.setText(adapterName);
+    }
+
+    private void findDevices() {
+
+        setAdapterName();
+
+        if (DEBUG) {
+            Log.d(TAG, "finding devices");
+        }
+
+        // MAKE US DISCOVERABLE
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+        startActivity(discoverableIntent);
+
+        // GET PAIRED DEVICES
+        availableDevices = new HashSet<>();
+        for (BluetoothDevice btd : mBluetoothAdapter.getBondedDevices()) {
+            availableDevices.add(btd);
+        }
+        displayableDevices = convertDevicesToStrings(availableDevices);
+        displayDevicesList(displayableDevices);
+
+        if (DEBUG) {
+            logList("paired devices", displayableDevices);
+        }
+
         // FIND DISCOVERABLE DEVICES
         // Register the BroadcastReceiver
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
         mBluetoothAdapter.startDiscovery();
-
-        // MAKE US DISCOVERABLE
-        Intent discoverableIntent = new
-                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        startActivity(discoverableIntent);
     }
 
-    private void findDevices() {
-        findPairedDevices();
-
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item_device, devices);
-//        deviceListView.setAdapter(arrayAdapter);
+    /**
+     * Returns a display string from a bluetooth device.
+     *
+     * @param device
+     * @return
+     */
+    private String deviceToString(BluetoothDevice device) {
+        return device.getName() + "\n" + device.getAddress();
     }
 
-    private List<String> findPairedDevices() {
-        ArrayList<String> devices = new ArrayList<>();
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        Log.d(TAG, "Number of paired devices: " + pairedDevices.size());
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            Log.d(TAG, "Went inside if statement");
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                devices.add(device.getName() + "\n" + device.getAddress());
-                Log.d(TAG, device.getName() + "\n" + device.getAddress());
-            }
+    /**
+     * Returns a list of strings representing each device from a set of Bluetooth devices
+     *
+     * @param bluDevices
+     * @return
+     */
+    private List<String> convertDevicesToStrings(Set<BluetoothDevice> bluDevices) {
+        List<String> devices = new ArrayList<>();
+        for (BluetoothDevice device : bluDevices) {
+            devices.add(deviceToString(device));
+            Log.d(TAG, deviceToString(device));
         }
         return devices;
     }
 
-    private List<String> findDiscoverableDevices() {
-
-
-        return null;
+    private void logList(String name, List<String> list) {
+        if (list.size() < 1) {
+            Log.d(TAG, "No items in list: " + name);
+            return;
+        }
+        for (String s : list) {
+            Log.d(TAG, s);
+        }
     }
 
     @Override
