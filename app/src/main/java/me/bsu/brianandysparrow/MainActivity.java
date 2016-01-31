a81e162f8e1a5ce6b08815cc7a5e349d197d59cc
Brian Su <briansuaccs@gmail.com>
2016-01-31 13:26:20 -0800

    find devices implemented

--- app/src/main/java/me/bsu/brianandysparrow/MainActivity.java
+++ app/src/main/java/me/bsu/brianandysparrow/MainActivity.java
@@ -2,39 +2,60 @@ package me.bsu.brianandysparrow;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "me.bsu.MainActivity";
 
+    TextView macAddressTextView;
     EditText messageEditText;
     Button sendMessageButton;
     ListView deviceListView;
-    ArrayAdapter<String> mArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item_device);
 
     BluetoothAdapter mBluetoothAdapter;
     private boolean bluetoothReady = false;
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                Log.d(TAG, "Discoverable device: " + device.getName() + "\n" + device.getAddress());
+                //                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        macAddressTextView = (TextView) findViewById(R.id.my_mac_address_text_view);
+        macAddressTextView.setText(getMacAddress());
         messageEditText = (EditText) findViewById(R.id.message_edittext);
         sendMessageButton = (Button) findViewById(R.id.message_send_button);
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         setupBluetooth();
+
+
 
     }
 
@@ -81,9 +104,28 @@ public class MainActivity extends AppCompatActivity {
         } else {
             findDevices();
         }
+        // FIND DISCOVERABLE DEVICES
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+        mBluetoothAdapter.startDiscovery();
+
+        // MAKE US DISCOVERABLE
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
     }
 
     private void findDevices() {
+        findPairedDevices();
+
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item_device, devices);
+//        deviceListView.setAdapter(arrayAdapter);
+    }
+
+    private List<String> findPairedDevices() {
+        ArrayList<String> devices = new ArrayList<>();
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         Log.d(TAG, "Number of paired devices: " + pairedDevices.size());
         // If there are paired devices
@@ -92,20 +134,27 @@ public class MainActivity extends AppCompatActivity {
             // Loop through paired devices
             for (BluetoothDevice device : pairedDevices) {
                 // Add the name and address to an array adapter to show in a ListView
-                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getName() + "\n" + device.getAddress());
                 Log.d(TAG, device.getName() + "\n" + device.getAddress());
             }
-//            deviceListView.setAdapter(mArrayAdapter);
         }
+        return devices;
+    }
+
+    private List<String> findDiscoverableDevices() {
+
+
+        return null;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Toast.makeText(this, "Result OK: " + (resultCode == RESULT_OK), Toast.LENGTH_SHORT).show();
-        if (resultCode == RESULT_OK) {
+        Toast.makeText(this, "Request Code: " + requestCode + " Result OK: " + (resultCode == RESULT_OK), Toast.LENGTH_SHORT).show();
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
             bluetoothReady = true;
+            setupBluetooth();
         }
-        setupBluetooth();
     }
+
 }
