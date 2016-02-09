38c10cfb018e4886850428c873f532a7f52613dd
Brian Su <briansuaccs@gmail.com>
2016-02-08 17:27:04 -0800

    added database

---
+++ app/src/main/java/me/bsu/brianandysparrow/models/DBVectorClockItem.java
@@ -0,0 +1,34 @@
+package me.bsu.brianandysparrow.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "DBVectorClockItems")
+public class DBVectorClockItem extends Model {
+
+    @Column(name = "UUID")
+    public String uuid;
+
+    @Column(name = "clock")
+    public int clock;
+
+    @Column(name = "DBTweet")
+    public DBTweet dbTweet;
+
+    public DBVectorClockItem() {
+        super();
+    }
+
+    public DBVectorClockItem(String uuid, int clock, DBTweet dbTweet) {
+        this.uuid = uuid;
+        this.clock = clock;
+        this.dbTweet = dbTweet;
+    }
+
+    public DBVectorClockItem(String uuid, DBTweet dbTweet) {
+        this.uuid = uuid;
+        this.clock = 0;
+        this.dbTweet = dbTweet;
+    }
+}
